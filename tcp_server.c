a180e4f27c6d1b78f1622c09e7bb6999c6d7cf36
Trezen Parmar <trezen7984891023@gmail.com>
2022-06-15 11:58:38 +0530

    Update tcp_server.c

--- tcp_server.c
+++ tcp_server.c
@@ -9,7 +9,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define MAX 100
+#define MAX  65536
 #define PORT 8080
 
 // func to chat between client and server
@@ -18,30 +18,55 @@ void chat_func(int connfd)
 {
 	char buff[MAX];
 	int flag = 1;
+	int infi_flag = 1;
 
-	printf("\n************ Server *************\n\n");
+	int *len;
+	printf("\n************ TCP Server *************\n\n");
 
-	while(1) 
+	while(infi_flag) 
 	{
 		while(flag == 1)
 		{
 			memset(buff, '\0', MAX);
-			
-			if((read(connfd, buff, MAX)) == -1)
+			int i = 0;
+			len = &i;
+
+			// read len of recieved data
+			if((read(connfd, len, sizeof(len))) == -1)
 			{
-				printf("failed to read from client \n");
+				printf("failed to read data\n");
 				close(connfd);
 				exit(0);
 			}
 
+
+
+			// read data from buffer
+			if((read(connfd, buff, MAX)) == -1)
+			{
+				printf("failed to read from client \n");
+				infi_flag = 0;
+				close(connfd);
+				break;
+			}
+			
+			
 			if(strlen(buff) == 1)
 			{
 				flag = 0;
 				break;
 			}
 
-			printf("\nClient : %s", buff);
 
+
+			printf("buffer length = %d\n", *len);
+			printf("\nClient : %s", buff);
+			
+			if(!strncmp("bye", buff,  3))
+			{
+				printf("Chat ended...!\n");
+				exit(0);
+			}
 		}
 
 		while(flag == 0)
@@ -49,22 +74,42 @@ void chat_func(int connfd)
 			memset(buff, '\0', MAX);
 			
 			printf("\nServer : ");
-			
+
+			// input data into buffer
 			int i=0;
 			while((buff[i++] = getchar()) != '\n')
 				;
+			
+			len = &i;
 
+			//check boundary condition
+			if((write(connfd, len, sizeof(len))) == -1)
+			{
+				printf("error in writting length\n");
+				close(connfd);
+				exit(0);
+			}
+
+			// write data into buffer
 			if((write(connfd, buff, MAX)) == -1)
 			{
 				printf("failed to write \n");
 				close(connfd);
+				infi_flag = 0;
 				exit(0);
 			}
 
 			if(strlen(buff) == 1)
 			{
 				flag = 1;
+				break;
 			}
+
+			if(!strncmp("bye", buff,  3))
+			{
+				printf("Chat ended...!\n");
+				exit(0);
+			}	
 		}			
 	}
 }
@@ -78,15 +123,14 @@ int main()
 
 	// socket create and verification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	
+
 	if (sockfd == -1) {
 		printf("socket creation failed...\n");
-		close(sockfd);
 		exit(0);
 	}
 	else
 		printf("Socket successfully created..\n");
-		
+
 	memset(&servaddr, '\0', sizeof(servaddr));
 
 	// assign IP, PORT
@@ -111,24 +155,26 @@ int main()
 	}
 	else
 		printf("Server listening..\n");
-		
+
 	len = sizeof(cli);
 
 	// Accept the data from client
 	connfd = accept(sockfd, (struct sockaddr*)&cli, &len);
-	
+
 	if (connfd < 0) {
-		
+
 		printf("server accept failed...\n");
-		close(sockfd);
+		close(connfd);
 		exit(0);
 	}
 	else
+	{
 		printf("server accepted the client...\n");
 
-	// Function to chat between client and server
-	chat_func(connfd);
-
+		// Function to chat between client and server
+		chat_func(connfd);
+	}
 	// After chatting close the socket
 	close(sockfd);
+	close(connfd);
 }
